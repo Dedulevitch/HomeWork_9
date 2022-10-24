c84947ea9c3ee44bf638ce425b2ae2c4464ed29b
Anton <dedulevitchanton@gmail.com>
2022-10-24 19:09:24 +0300

    HomeWork

---
+++ Task_64/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+Console.Clear();
+Console.WriteLine("Задайте значение N ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"N = {n}");
+NumbersRec2(n);
+
+
+
+void NumbersRec2(int n)
+{
+    if (n==0) return;
+    Console.Write($"{n}" + "\t");
+    n--;
+    NumbersRec2(n);
+}
---
+++ Task_66/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+Console.Clear();
+Console.Write("Задайте элемент M => ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Задайте элемент N => ");
+int b = int.Parse(Console.ReadLine());
+int x = NaturSum(a, b);
+Console.WriteLine($"Сумма => {x}");
+
+
+
+int NaturSum(int a, int b)
+{
+    if (a == b+1) return 0;
+    else return a + NaturSum(a+1,b);
+
+}
+
